a4ac9a2ef47d1c44c1d647de576607329ce3bc79
Van Mc G <00van12@gmail.com>
2018-07-14 21:56:56 +0000

    Added tests for rendering templates

--- run.py
+++ run.py
@@ -5,18 +5,32 @@ import json
 from flask import Flask, redirect, render_template, request, flash, url_for, session, jsonify
 
 app = Flask(__name__)
+#5 append messages
+# def add_messages(username, message):
+#     messages.append ("{0}: {1}\n".format(username, message))
+
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
+    return render_template("index.html")
     
-    return"<h1>Hello World!</h1>"
     
     
 @app.route('/leader_board', methods=["GET", "POST"])
 def leader_board():
+    return render_template("leader_board.html")
     
-    return"<h1>You are leading!</h1>"  
-    
+ 
+@app.route('/<username>', methods=["GET", "POST"])
+def user(username):
+    return username
+ 
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    return "{0}: {1}\n".format(username, message)
+ 
+   
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'), port=int(os.environ.get('PORT', 0)), debug=True)
 
